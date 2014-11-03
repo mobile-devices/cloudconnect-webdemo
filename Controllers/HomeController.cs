8ac98f876b32147038261066655f9f6ae9b3af77
Cyril schmitt <cyril.schmitt@mobile-devices.fr>
2014-11-03 14:12:22 +0100

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CloudConnect.CouchBaseProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudConnect.WebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            NotificationProvider np = new NotificationProvider();
+            IEnumerable<Notification> data = np.RequestNotificationCache("GENERIC", DateTime.UtcNow);
+
+            Console.WriteLine(data.Count());
+            //CloudConnect.CouchBaseProvider.CouchbaseManager.RegisterModelViews(new Assembly[] { Assembly.GetAssembly(typeof(CloudConnect.CouchBaseProvider.CouchbaseManager)) });
+            //NotificationProvider np = new NotificationProvider();
+             
+            //System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+            //timer.Start();
+            //for (int i = 1; i < 10000; i++)
+            //{
+            //    np.PushNotificationCache("GENERIC", "test" + i.ToString(), DateTime.UtcNow);
+            //}
+            //timer.Stop();
+
+
+            //2302
+            return View();
+        }
+    }
+}
---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,20 @@
+﻿using CloudConnect.CouchBaseProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CloudConnect.WebDemo.Controllers
+{
+    public class NotificationsController : ApiController
+    {
+        // POST api/notifications
+        public void Post([FromBody]string value)
+        {
+            NotificationProvider np = new NotificationProvider();
+            np.PushNotificationCache("GENERIC", value, DateTime.UtcNow);
+        }
+    }
+}
